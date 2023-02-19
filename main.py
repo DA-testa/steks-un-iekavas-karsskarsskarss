df5793f8a23f3ee05af20ead1650d27867f79806
karsskarsskarss <ajkarthurs@gmail.com>
2023-02-19 22:33:39 +0200

    upd10

--- main.py
+++ main.py
@@ -11,19 +11,23 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, i+1))
             pass
 
         if next in ")]}":
-            # Process closing bracket, write your code here
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                return i + 1
+            opening_brackets_stack.pop()
             pass
-
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    return "Success"
 
 def main():
+    teksts1 = input("xd\n")
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-
+    
 
 if __name__ == "__main__":
     main()
