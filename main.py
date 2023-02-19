abcb6dfe14f7dbb9c73e5258d843051d195a8079
karsskarsskarss <ajkarthurs@gmail.com>
2023-02-19 22:38:37 +0200

    upd12

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Bracket = namedtuple("Bracket", ["char", "position"])
 
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
+
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
@@ -14,12 +15,25 @@ def find_mismatch(text):
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i + 1
             opening_brackets_stack.pop()
-            
+
     if opening_brackets_stack:
         return opening_brackets_stack[0].position
     return "Success"
+
 def main():
-    teksts1 = input("xd\n")
-    text = input()
-    mismatch = find_mismatch(text)
-    print(mismatch)
+    input_choice = input("Enter 'F' to input from a file or 'I' to input from the console: ")
+    if "F" in input_choice:
+        file_path = input("Enter the path to the file: ")
+        with open(file_path, "r") as f:
+            text = f.read()
+            mismatch = find_mismatch(text)
+            print(mismatch)
+    elif "I" in input_choice:
+        text = input("Enter the string to check: ")
+        mismatch = find_mismatch(text)
+        print(mismatch)
+    else:
+        print("Invalid input choice.")
+
+if __name__ == "__main__":
+    main()
