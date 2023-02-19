454f8010d0716de4a9bfe4081caa203af367e7ac
karsskarsskarss <ajkarthurs@gmail.com>
2023-02-19 21:03:00 +0200

    update 1

--- main.py
+++ main.py
@@ -13,19 +13,45 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, i))
             pass
 
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack:
+                return i + 1
+            last_open = opening_brackets_stack.pop()
+            if not are_matching(last_open.char, next):
+                return i + 1
+
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position + 1
+    
+    return 'Success'
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    input_choice = input("Ievadiet F vai I: ")
+    if "F" in input_choice:
+        file_path = input("Ievadiet ceļu lidz failam: ")
+        with open(file_path, "r") as f:
+            text = f.read()
+            mismatch = find_mismatch(text)
+            if mismatch == 'Success':
+                print("Success")
+            else:
+                print(mismatch)
+    elif "I" in input_choice:
+        text = input()
+        mismatch = find_mismatch(text)
+        if mismatch == 'Success':
+            print("Success")
+        else:
+            print(mismatch)
+    else:
+        print("Invalid input choice.")
 
 
 if __name__ == "__main__":
     main()
+
+
