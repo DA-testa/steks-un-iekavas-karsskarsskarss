60c6df0b9b7cfe8a82a8d48f743dba0f27cde904
karsskarsskarss <ajkarthurs@gmail.com>
2023-02-19 21:39:26 +0200

    upd3

--- main.py
+++ main.py
@@ -1,59 +1,50 @@
-# python3
-
 from collections import namedtuple
+from typing import List, Union
 
-Bracket = namedtuple("Bracket", ["char", "position"])
+Bracket = namedtuple("Bracket",["char", "position"])
 
-
-def are_matching(left, right):
+def are_matching(left: str, right: str) -> bool:
     return (left + right) in ["()", "[]", "{}"]
 
-
-def find_mismatch(text):
-    opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next  == '\n':
-            countinue 
-        if next in "([{":
-            opening_brackets_stack.append(Bracket(next, i + 1))
-            
-
-        if next in ")]}":
-            if not opening_brackets_stack:
+def find_mismatch(text: str) -> Union[str, int]:
+    def recurse(i: int, stack: List[Bracket]) -> Union[str, int]:
+        if not text:
+            return 'Success'
+        elif text[0] in "([{":
+            return recurse(i+1, [(text[0], i)] + stack)
+        elif text[0] in ")]}":
+            if not stack:
                 return i + 1
-            last_open = opening_brackets_stack.pop()
-            if not are_matching(opening_brackets_stack[-1].char, next):
+            last_open = stack[0]
+            if not are_matching(last_open[0], text[0]):
                 return i + 1
-            opening_brackets_stack.pop()
+            else:
+                return recurse(i+1, stack[1:])
+        else:
+            return recurse(i+1, stack)
+    return recurse(0, [])
 
-    if opening_brackets_stack:
-        return opening_brackets_stack[0].position 
-    
-    return 'Success'
-
-
-def main():
-    text = input()
-    if"F" in text:
-        for i in range(6):
-            with open(f"test/{i}") as f:
-                text = f.read()
-                mismatch = find_mismatch(text)
+def main() -> None:
+    input_choice = input("Ievadiet F vai I: ")
+    if "F" in input_choice:
+        file_path = input("Ievadiet ceļu lidz failam: ")
+        with open(file_path, "r") as f:
+            text = f.read()
+            mismatch = find_mismatch(text)
+            if mismatch == 'Success':
+                print("Success")
+            else:
                 print(mismatch)
-                if "I" in text:
-                    text = input()
-                    mismatch = find_mismatch(text)
-                    print(mismatch)
+    elif "I" in input_choice:
+        text = input()
+        mismatch = find_mismatch(text)
+        if mismatch == 'Success':
+            print("Success")
+        else:
+            print(mismatch)
+    else:
+        print("Invalid input choice.")
+
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
